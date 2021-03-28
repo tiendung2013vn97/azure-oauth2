3581143127ae0bc23cd4fc882daa2525954a4aab
QDuy <phamquangduy19@gmail.com>
2021-03-28 21:50:29 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/api/azure-oauth2-callback", (req, res) => {
+	console.info(`Receive oauth2 callback from azure: %j`, req.query);
+	res.end("ok");
+});
+
+app.get("/api/health",(req,res)=>{
+	res.end("I'am fine.")
+})
+
+app.use("/",(req,res,next)=>{
+	res.send("404: api not exist")
+})
+
+app.listen(3000, (err) => {
+	console.info(`Server running on port `, 3000);
+});
